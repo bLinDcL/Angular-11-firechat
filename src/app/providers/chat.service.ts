b73d854f3009d4b378382c9e1cbcc445b10b3c9c
FVergaraA <fe.vergaraa@gmail.com>
2019-07-12 12:38:32 -0400

    Autenticacion con Google

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Mensaje } from "../interface/mensaje.interface";
 import { map } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,22 @@ import { map } from 'rxjs/operators';
 export class ChatService {
   private itemColletion: AngularFirestoreCollection<Mensaje>;
   public chats: Mensaje[] = [];
+  public usuario: any = {};
 
-  constructor( private angularFireStore: AngularFirestore) {}
+  constructor( private angularFireStore: AngularFirestore, public angularFireAuth: AngularFireAuth ) {
+    this.angularFireAuth.authState.subscribe( user => {
+      console.log('Estado del usuario: ', user);
+    });
+  }
+
+  login( proveedor: string ) {
+    if (proveedor === 'Google') {
+      this.angularFireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    }
+  }
+  logout() {
+    this.angularFireAuth.auth.signOut();
+  }
 
   cargarMensajes(){
     this.itemColletion = this.angularFireStore.collection<Mensaje>('chats', ref => ref.orderBy('fecha', 'desc').limit(10) );
@@ -23,7 +39,7 @@ export class ChatService {
     }));
   }
 
-  agregarMensaje(texto: string) {
+  agregarMensaje( texto: string ) {
     let mensaje: Mensaje = {
       nombre: 'Felipe',
       mensaje: texto,
